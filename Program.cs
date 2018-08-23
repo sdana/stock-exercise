5ee44aed1c285d10c70bcd5fcaf8be7d3720f20d
Seth Dana <seth.dana@gmail.com>
2018-08-22 20:00:55 -0500

    Working, need to add full company name to stock valuation list

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stockPurchaseDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> stocks = new Dictionary<string, string>(){
+                {"GM", "General Motors"},
+                {"CAT", "Caterpillar"},
+                {"APL", "Apple Computer"},
+                {"GOOG", "Alphabet"},
+                {"COK", "Coca-Cola"}
+            };
+
+            List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>()
+            {
+                {("GM", 50, 49.99)},
+                {("APL", 100, 3000.00)},
+                {("COK", 49, 30.04)},
+                {("CAT", 25, 29.65)},
+                {("GOOG", 5000, 800.94)}
+            };
+
+            Dictionary<string, double> stockValues = new Dictionary<string, double>();
+
+            foreach ((string ticker, int shares, double price) stock in purchases)
+            {
+                if (stocks.ContainsKey(stock.ticker))
+                {
+                    double stockValuation = (stock.shares * stock.price);
+                    stockValues[stock.ticker] = stockValuation;
+                }
+                else 
+                {
+                    double stockValuation = (stock.shares * stock.price);
+                    stockValues.Add(stock.ticker, stockValuation);
+                }
+            }
+
+            foreach (KeyValuePair<string, double> stockThing in stockValues)
+            {
+                System.Console.WriteLine(stockThing);
+            }
+
+        }
+    }
+}
